212d8442d0848f9b9f2ac532f713a5a29332921d
jin zhao <jinzhaocnfr@gmail.com>
2024-12-13 17:30:07 +0100

    added cardview of birthdate picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
 
 dependencies {
 
+    implementation ("com.google.android.material:material:1.12.0")
+
 
     // ViewModel & LiveData
     implementation ("androidx.fragment:fragment-ktx:1.8.5")
--- app/src/main/java/com/example/p8vitesse/ui/add/AddCandidatActivity.kt
+++ app/src/main/java/com/example/p8vitesse/ui/add/AddCandidatActivity.kt
@@ -1,13 +1,17 @@
 package com.example.p8vitesse.ui.add
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.p8vitesse.R
+import java.util.Calendar
 
 class AddCandidatActivity : AppCompatActivity() {
 
@@ -17,7 +21,7 @@ class AddCandidatActivity : AppCompatActivity() {
 
         // Find the toolbar by its ID
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-
+        val editText = findViewById<EditText>(R.id.editTextDateOfBirth)
         // Set the toolbar as the app's action bar
         setSupportActionBar(toolbar)
 
@@ -26,6 +30,8 @@ class AddCandidatActivity : AppCompatActivity() {
 
         // Set the title of the activity
         supportActionBar?.title = "Activity Title"
+
+        showDatePicker(editText)
     }
 
     // Handle the "up" button click event
@@ -39,4 +45,28 @@ class AddCandidatActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun showDatePicker(editTextDateOfBirth: EditText) {
+        // Get the current date
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Create DatePickerDialog
+        val datePickerDialog = DatePickerDialog(editTextDateOfBirth.context, { _, selectedYear, selectedMonth, selectedDay ->
+            // Format the selected date as jj/mm/aaaa
+            val formattedDate = "${selectedDay}/${selectedMonth + 1}/$selectedYear"
+            editTextDateOfBirth.setText(formattedDate)
+        }, year, month, day)
+
+        // Restrict date picker to not allow selecting a future date
+        datePickerDialog.datePicker.maxDate = calendar.timeInMillis
+        // Show the DatePickerDialog when the EditText is clicked
+
+        findViewById<ImageView>(R.id.calendaricon).setOnClickListener {
+            datePickerDialog.show()
+        }
+    }
+
 }
